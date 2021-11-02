02a68cabdf1cd739e5b0c9080fb7ea8b6aae35b1
James Armes <jamesleearmes@gmail.com>
2021-11-01 22:22:24 -0600

    Add utility functions to Vector3

--- src/Vector.h
+++ src/Vector.h
@@ -3,6 +3,7 @@
 
 #include <cmath>
 #include <concepts>
+#include <iostream>
 #include <type_traits>
 
 template<std::floating_point T>
@@ -43,13 +44,30 @@ class Vector3
             return *this;
         }
 
-        Vector3& operator/=(T rhs)
+        Vector3& operator/=(T rhs) // TODO: Re-evaluate pass-by-value
         {
             x() *= rhs;
             y() *= rhs;
             z() *= rhs;
 
             return *this;
+        }
+
+        bool operator==(const Vector3<T>& other)
+        {
+            return other.x() == this->x() &&
+                   other.y() == this->y() &&
+                   other.z() == this->z();
+        }
+
+        bool operator!=(const Vector3<T>& other)
+        {
+            return !(*this == other);
+        }
+
+        Vector3<T> toUnit() const
+        {
+            (*this)/this->length();
         }
 
         T length() const
@@ -62,5 +80,79 @@ class Vector3
     private:
         T data_[3];
 };
+
+// Utility functions
+template<typename T>
+std::ostream& operator<<(std::ostream& out, const Vector3<T>& vector)
+{
+    return out << vector.x() << ", " << vector.y() << ", " << vector.z();
+}
+
+template<typename T>
+Vector3<T> operator+(const Vector3<T>& v1, const Vector3<T>& v2)
+{
+    return Vector3{ v1.x() + v2.x(),
+                    v1.y() + v2.y(),
+                    v1.z() + v2.z()};
+}
+
+template<typename T>
+Vector3<T> operator-(const Vector3<T>& v1, const Vector3<T>& v2)
+{
+    return Vector3{ v1.x() - v2.x(),
+                    v1.y() - v2.y(),
+                    v1.z() - v2.z()};
+}
+
+template<typename T>
+Vector3<T> operator*(const Vector3<T>& v1, const Vector3<T>& v2)
+{
+    return Vector3{ v1.x() * v2.x(),
+                    v1.y() * v2.y(),
+                    v1.z() * v2.z()};
+}
+
+template<typename T, std::floating_point U>
+Vector3<T> operator*(const Vector3<T>& v1, U scalar)
+{
+    // The cast is needed to ensure if U is of type 'double'
+    // and T is of type 'float', that we return Vector3<float>
+    // vs. Vector3<double> as would normally happen via promotion
+    return Vector3{
+        static_cast<T>(v1.x() * scalar),
+        static_cast<T>(v1.y() * scalar),
+        static_cast<T>(v1.z() * scalar)
+    };
+}
+
+template<typename T, std::floating_point U>
+Vector3<T> operator*(U scalar, const Vector3<T>& v1 )
+{
+    return v1*scalar;
+}
+
+template<typename T, std::floating_point U>
+Vector3<T> operator/(Vector3<T> v1, U scalar)
+{
+    return (1.0/scalar) * v1;
+}
+
+template<typename T>
+T dot(const Vector3<T>& v1, const Vector3<T>& v2)
+{
+    return (v1.x() * v2.x() +
+            v1.y() * v2.y() +
+            v1.z() * v2.z());
+}
+
+template<typename T>
+Vector3<T> cross(const Vector3<T>& v1, const Vector3<T>& v2)
+{
+    return Vector3(
+        v1.y() * v2.z() - v1.z() * v2.y(),
+        v1.z() * v2.x() - v1.x() * v2.z(),
+        v1.x() * v2.y() - v1.y() * v2.x()
+    );
+}
 
 #endif // VECTOR3_H
