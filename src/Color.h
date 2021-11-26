dd203621d6dee154855d128949ded1c6a531e0da
James Armes <jamesleearmes@gmail.com>
2021-11-25 21:02:52 -0700

    Does not compile; need to use std::lerp

--- src/Color.h
+++ src/Color.h
@@ -1,11 +1,14 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-struct Color
-{
-    std::uint8_t r;
-    std::uint8_t g;
-    std::uint8_t b;
+struct Color {
+  std::uint8_t r;
+  std::uint8_t g;
+  std::uint8_t b;
+
+  Color(std::uint8_t r, std::uint8_t g, std::uint8_t b) : r(r), g(g), b(b) {}
+
+  Color() {}
 };
 
 #endif // COLOR_H
--- src/Ray.h
+++ src/Ray.h
@@ -1,22 +1,29 @@
 #ifndef RAY_H
 #define RAY_H
 
-#include "Point.h"
+#include "Color.h"
 #include "Vector.h"
 
-class Ray
-{
-public:
-    Point3f getOrigin() const { return origin_; }
-    Vector3f getDirection() const { return direction_; }
+#include <cmath>
 
-    Point3f at(float t) const {
-        return origin_ + t * direction_;
-    }
+class Ray {
+public:
+  Point3f getOrigin() const { return origin_; }
+  Vector3f getDirection() const { return direction_; }
+
+  Point3f at(float t) const { return origin_ + t * direction_; }
+
+  Color getColor() const {
+    Vector3f unit_direction = direction_.toUnit();
+    auto t = (0.5 * unit_direction.y() + 1.0);
+
+    Color rayColor = std::lerp() // TODO: use this
+        (1.0 - t) * Color{1, 1, 1} + t * Color{0.5, 0.7, 1.0};
+  }
 
 private:
-    Point3f origin_;
-    Vector3f direction_;
+  Point3f origin_;
+  Vector3f direction_;
 };
 
 #endif
