d7fbc7b35aacdd69ef7d6f9c5b2b975f0e6fee6e
James Armes <jamesleearmes@gmail.com>
2022-09-19 21:33:13 -0600

    Add original files

--- src/Color.h
+++ src/Color.h
@@ -1,6 +1,8 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include <iostream>
+
 struct Color
 {
   std::uint8_t r;
@@ -8,6 +10,20 @@ struct Color
   std::uint8_t b;
 
   constexpr Color(std::uint8_t r, std::uint8_t g, std::uint8_t b) noexcept : r(r), g(g), b(b) {}
+
+  Color &operator*(double scale)
+  {
+    if (scale > 1.0 || scale < 0.0)
+    {
+      std::cerr << "Invalid scale value!" << std::endl;
+    }
+
+    r = static_cast<std::uint8_t>(r * scale);
+    g = static_cast<std::uint8_t>(g * scale);
+    b = static_cast<std::uint8_t>(b * scale);
+
+    return *this;
+  }
 
   Color() {}
 };
--- src/Ray.h
+++ src/Ray.h
@@ -9,20 +9,20 @@
 class Ray
 {
 public:
-  Point3f getOrigin() const { return origin_; }
-  Vector3f getDirection() const { return direction_; }
+  [[nodiscard]] Point3f getOrigin() const noexcept { return origin_; }
+  [[nodiscard]] Vector3f getDirection() const noexcept { return direction_; }
 
-  Point3f at(float t) const { return origin_ + t * direction_; }
+  [[nodiscard]] Point3f at(float t) const noexcept { return origin_ + t * direction_; }
 
-  Color getColor() const
+  [[nodiscard]] Color getColor() const noexcept
   {
     Vector3f unit_direction = direction_.toUnit();
     auto t = (0.5 * unit_direction.y() + 1.0);
 
-    Color rayColor = std::lerp() // TODO: use this
-                         (1.0 - t) *
-                         Color{1, 1, 1} +
-                     t * Color{0.5, 0.7, 1.0};
+    Color white {1, 1, 1};
+    Color blue {0.5, 0.7, 1.0};
+
+    Color rayColor = (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
   }
 
 private:
